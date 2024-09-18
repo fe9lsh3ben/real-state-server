ab5cda0109906f26ae9a1ad6f39b445203387870
faisal <fe9lsh3ben@gmail.com>
2024-09-18 16:37:16 +0300

    codes formatting

--- prismaDB_utilities/Ts&Cs_utilities.js
+++ prismaDB_utilities/Ts&Cs_utilities.js
@@ -3,88 +3,84 @@
 
 const find_last_termsANDconditions = async (prisma, Committed_By, req) => {
 
-   
-    var result 
+
+    var result
     var TC_type
     var TC_ID_type
     try {
-        
-        switch(req.body.CommittedBy){
 
-        case("OFFICE_OWNER"):
-            TC_ID_type = 'OO'
-            TC_type = Committed_By.OFFICE_OWNER
-        break;
+        switch (req.body.CommittedBy) {
 
-        case("OFFICE_STAFF"):
-            TC_ID_type = 'OS'
-            TC_type = Committed_By.OFFICE_STAFF
-        break;
+            case ("OFFICE_OWNER"):
+                TC_ID_type = 'OO'
+                TC_type = Committed_By.OFFICE_OWNER
+                break;
 
-        case("BENEFICIARY"):
-            TC_ID_type = 'B'
-            TC_type = Committed_By.BENEFICIARY
-        break;
+            case ("OFFICE_STAFF"):
+                TC_ID_type = 'OS'
+                TC_type = Committed_By.OFFICE_STAFF
+                break;
 
-        case("BUSINESS_BENEFICIARY"):
-            TC_ID_type = 'BB'
-            TC_type = Committed_By.BUSINESS_BENEFICIARY
-        break;
-        
-    
-    
-    }
-        
+            case ("BENEFICIARY"):
+                TC_ID_type = 'B'
+                TC_type = Committed_By.BENEFICIARY
+                break;
+
+            case ("BUSINESS_BENEFICIARY"):
+                TC_ID_type = 'BB'
+                TC_type = Committed_By.BUSINESS_BENEFICIARY
+                break;
+
+
+
+        }
+
         await prisma.termsAndCondetions.findMany({
-            where:{ID: {contains: TC_ID_type}},orderBy: { ID: 'desc'},take: 1,
+            where: { ID: { contains: TC_ID_type } }, orderBy: { ID: 'desc' }, take: 1,
         }).then((v) => {
             result = v[0];
         })
 
-        
+
         if (result == undefined) {
             // console.log("in if")
             result = await prisma.termsAndCondetions.create({
-                data:{ ID: TC_ID_type + "_000001", Content:[{"1":req.body.Content}],
-                CommittedBy: TC_type,
-                MadeBy: req.body.MadeBy}
+                data: {
+                    ID: TC_ID_type + "_000001", Content: [{ "1": req.body.Content }],
+                    CommittedBy: TC_type,
+                    MadeBy: req.body.MadeBy
+                }
             })
-        }else{
-            
+        } else {
+
             const extractedStrings = result.ID.match(/^([A-Za-z_]+)(\d+)$/);
             const IDnumberedPart = extractedStrings[2]
             const incremented = (parseInt(extractedStrings[2], 10) + 1).toString()
             const paddedIncrement = incremented.padStart(IDnumberedPart.length, '0')
-            
+
             result = await prisma.termsAndCondetions.create({
-                data:{ 
-                    ID:  `${extractedStrings[1]}${paddedIncrement}`,
-                    Content:[{"1":req.body.Content}],
+                data: {
+                    ID: `${extractedStrings[1]}${paddedIncrement}`,
+                    Content: [{ "1": req.body.Content }],
                     CommittedBy: TC_type,
                     MadeBy: req.body.MadeBy
                 }
             })
 
-            
+
         }
         console.log(result)
         return result;
 
     } catch (err) {
-        
+
         throw err;
-        
+
     }
-    
-      
-    
-   
-    
-  
-   
-   
+
+
 }
 
 
 
-module.exports = {find_last_termsANDconditions}
+module.exports = { find_last_termsANDconditions }
