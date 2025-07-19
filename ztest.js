e4ef33da0cefeea7709376d1cbe27b24e5a745ba
Faisal Mohammed <fe9lsh4ben@gmail.com>
2025-07-19 22:49:55 +0300

    test file added

---
+++ ztest.js
@@ -0,0 +1,4 @@
+//docker run --name my-postgres -e POSTGRES_USER=myuser -e POSTGRES_PASSWORD=mypassword -e POSTGRES_DB=RE_DB -p 5432:5432 -d postgres
+
+
+//docker exec -it my-postgres psql -U myuser -d RE_DB
